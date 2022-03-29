4ec5c9dd517f5a794cbd3259cc85d5342ba40733
mwhalen18 <matthew.whalen18@gmail.com>
2022-03-29 16:42:14 -0400

    Dockerization succeeded

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # WeddingJukebox
 An app that will run the Spotify API via a RFID reader. As seen my my wedding...
+
+
+To run this container, first cloen this repository.
+
+## Add Spotify Credentials
+
+To setup Spotify API credentials...
+
+
+Next, add file `credentials.py` 
+
+```
+spotipy_credentials = {
+    'USERNAME': "user",
+    'SPOTIPY_CLIENT_ID': "your-spotify-client-id",
+    'SPOTIPY_CLIENT_SECRET': "your-spotfy-secret"
+}
+```
+
+## Run the container
+
+To run the container 
+
+```
+docker run --interactive --tty wedding-jukebox
+```
--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print('hello world')
+import spotipy
+from credentials import spotipy_credentials
+import spotipy.util as util
+
+username = spotipy_credentials['USERNAME']
+clientID = spotipy_credentials['SPOTIPY_CLIENT_ID']
+clientSecret = spotipy_credentials['SPOTIPY_CLIENT_SECRET']
+redirectURI = 'http://google.com'
+
+scope = 'user-read-private user-read-playback-state user-modify-playback-state playlist-modify-private'
+
+oauth_object = spotipy.SpotifyOAuth(clientID,clientSecret,redirectURI,scope=scope)
+
+token = util.prompt_for_user_token(username = username, client_id=clientID, client_secret=clientSecret, redirect_uri=redirectURI, scope=scope)
+
+# Create Spotify Object
+spotify = spotipy.Spotify(auth=token)
+user = spotify.current_user()
+
+#Get web browser to play tracks
+devices = spotify.devices()
+deviceID = devices['devices'][0]['id']
+
+#print(json.dumps(user,sort_keys=True, indent=4))
+playlisturi = "spotify:playlist:7DM159dKkjm0Mgu2gFARp0"
+
+spotify.start_playback(deviceID, playlisturi, None)
+print("Welcome to your Spotify Jukebox" + user['display_name'])
+tracks = []
+
+while True:
+    print("Welcome to your Spotify Jukebox" + user['display_name'])
+    print("Enter 'exit' to exit")
+    print("Enter a URI")
+    songURI = input("Enter a URI: ")
+    if songURI == "exit":
+        break
+    elif songURI in tracks:
+        pass
+    else:
+        spotify.user_playlist_add_tracks(username, "7DM159dKkjm0Mgu2gFARp0", [songURI])
+        tracks.append(songURI)
